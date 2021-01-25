c8787fbfa44165cad9bdb7cc242e4f27f0d044a8
Colin Edwards <ColinJhEdwards@gmail.com>
2021-01-24 20:59:27 -0800

    created a function that will update background color for present time (wip)

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 //Selectors
+const saveBtn = document.querySelector(".saveBtn");
+const currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
+const textArea = document.querySelectorAll("#todo");
+let currentHour = new Date().getHours();
+console.log(currentDay);
+console.log(textArea);
 
 //Event Listeners
+saveBtn.addEventListener("click", function () {
+  console.log("You did it");
+});
 
 //Functions
 
 var updateDate = function () {
-  document.getElementById("currentDay").innerHTML = moment().format(
-    "MMMM Do YYYY, h:mm:ss a"
-  );
+  document.getElementById("currentDay").innerHTML = currentDay;
 };
 setInterval(updateDate, 1000);
 updateDate();
+
+function timeblockColor() {
+  const timeBlock = document.querySelectorAll(".timeblock");
+
+  for (let i = 0; i < timeBlock.length; i++) {
+    const timeValue = timeBlock[i].dataset.time;
+    if (timeValue === "8") {
+      if (currentDay.includes("8:")) {
+        textArea[11].classList.add("present");
+      }
+    }
+  }
+}
+setInterval(timeblockColor, 1000);
+timeblockColor();
 
 // To Do
 
